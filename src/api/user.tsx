5127a484aeb55e4fedc8db88fce043b552b0e700
Kim, Yerim <yerim343@naver.com>
2024-07-15 16:55:05 +0900

    component 분리 #2

--- src/api/user.tsx
+++ src/api/user.tsx
@@ -1,41 +1,72 @@
-export const checkEmailAvailability = async (email: string): Promise<boolean> => {
-    const params = new URLSearchParams({ email });
-    const url = `http://localhost:8088/mysite6/user/api/checkemail?${params.toString()}`;
-  
-    try {
-      const response = await fetch(url);
-      console.log(response);
-      if (!response.ok) {
-        throw new Error('네트워크 응답이 올바르지 않습니다');
-      }
-      const result = await response.json();
-      return !result.data; 
-    } catch (error) {
-      console.error("이메일 중복 확인 중 오류 발생:", error);
-      throw error;
+export const checkEmailAvailability = async (
+  email: string
+): Promise<boolean> => {
+  const params = new URLSearchParams({ email });
+  const url = `http://localhost:8088/mysite6/user/api/checkemail?${params.toString()}`;
+
+  try {
+    const response = await fetch(url);
+    console.log(response);
+    if (!response.ok) {
+      throw new Error("네트워크 응답이 올바르지 않습니다");
     }
-  };
-  
-  export interface RegisterUserData {
-    name: string;
-    email: string;
-    password: string;
-    gender: string;
+    const result = await response.json();
+    return !result.data;
+  } catch (error) {
+    console.error("이메일 중복 확인 중 오류 발생:", error);
+    throw error;
   }
-  
-  export const registerUser = async (data: RegisterUserData): Promise<void> => {
-    try {
-      const response = await fetch("http://localhost:8088/mysite6/user/join", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(data),
-      });
-  
-      if (!response.ok) {
-        throw new Error('네트워크 응답이 올바르지 않습니다');
-      }
-    } catch (error) {
-      console.error("회원가입 중 오류 발생:", error);
-      throw error;
+};
+
+export interface RegisterUserData {
+  name: string;
+  email: string;
+  password: string;
+  gender: string;
+}
+
+export const registerUser = async (data: RegisterUserData): Promise<void> => {
+  try {
+    const response = await fetch("http://localhost:8088/mysite6/user/join", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      throw new Error("네트워크 응답이 올바르지 않습니다");
     }
-  };
+  } catch (error) {
+    console.error("회원가입 중 오류 발생:", error);
+    throw error;
+  }
+};
+
+export interface LoginResponse {
+  no: number;
+  name: string;
+  email: string;
+  gender: string;
+  role: "user" | "admin";
+}
+
+export const loginUser = async (
+  email: string,
+  password: string
+): Promise<LoginResponse> => {
+  const url = "http://localhost:8088/mysite6/user/auth";
+  const body = new URLSearchParams({ email, password });
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body : body.toString(),
+  });
+
+  if (!response.ok) {
+    throw new Error("로그인 실패");
+  }
+
+  const result: LoginResponse = await response.json();
+  return result;
+};
--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { useAuthStore } from "../../store/authStore";
+import InputField from "../inputs/InputField";
+import { loginUser, LoginResponse } from "../../api/user";
 
 interface LoginFormInput {
   email: string;
@@ -9,61 +11,49 @@ interface LoginFormInput {
 }
 
 const LoginForm: React.FC = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm<LoginFormInput>();
-    const navigate = useNavigate();
-    const login = useAuthStore((state) => state.login);
-    const [loginFailed, setLoginFailed] = useState(false);
-  
-    const onSubmit: SubmitHandler<LoginFormInput> = async (data) => {
-      try {
-        const response = await fetch('http://localhost:8088/mysite6/user/auth', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-          body: new URLSearchParams({
-            email: data.email,
-            password: data.password
-          }).toString()
-        });
-  
-        if (!response.ok) {
-            setLoginFailed(true);
-            return;
-          }
-    
-          const result = await response.json();
-          login({ ...result, password: undefined }); // 비밀번호는 제거
-          navigate('/'); // 로그인 성공 시 홈 페이지로 이동
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormInput>();
+  const navigate = useNavigate();
+  const login = useAuthStore((state) => state.login);
+  const [loginFailed, setLoginFailed] = useState(false);
 
-      } catch (error) {
-        console.error('로그인 실패:', error);
-        setLoginFailed(true);
-      }
-    };
-  
-    return (
-        <form id="login-form" onSubmit={handleSubmit(onSubmit)}>
-          <label className="block-label" htmlFor="email">이메일</label>
-          <input
-            id="email"
-            {...register('email', { required: true })}
-            type="text"
-          />
-          {errors.email && <p>이메일을 입력하세요</p>}
-  
-          <label className="block-label" htmlFor="password">패스워드</label>
-          <input
-            id="password"
-            {...register('password', { required: "패스워드를 입력하세요" })}
-            type="password"
-          />
-          {errors.password && <p>패스워드를 입력하세요</p>}
-  
-          {loginFailed && (
-            <p>로그인이 실패 했습니다.</p>
-          )}
-          <input type="submit" value="로그인" />
-        </form>
-    );
+  const onSubmit: SubmitHandler<LoginFormInput> = async (data) => {
+    try {
+      const result: LoginResponse = await loginUser(data.email, data.password);
+      login(result); 
+      navigate('/');
+    } catch (error) {
+      console.error("로그인 실패:", error);
+      setLoginFailed(true);
+    }
+   
   };
-  
-  export default LoginForm;
+
+  return (
+    <form id="login-form" onSubmit={handleSubmit(onSubmit)}>
+      <InputField<LoginFormInput>
+        id="email"
+        label="이메일"
+        type="text"
+        register={register}
+        validation={{ required: "이메일을 입력하세요" }}
+        error={errors.email}
+      />
+      <InputField<LoginFormInput>
+        id="password"
+        label="패스워드"
+        type="password"
+        register={register}
+        validation={{ required: "패스워드를 입력하세요" }}
+        error={errors.password}
+      />
+      {loginFailed && <p>로그인이 실패 했습니다.</p>}
+      <input type="submit" value="로그인" />
+    </form>
+  );
+};
+
+export default LoginForm;
